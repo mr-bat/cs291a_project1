deb7411693c86a9297e2cc38a8a2e9e77b312d78
Abtin Bateni <abtinbateni@gmail.com>
2019-10-07 11:02:45 -0700

    add gen_token endpoint - NOT TESTED

--- function.rb
+++ function.rb
@@ -7,7 +7,32 @@ require 'pp'
 def main(event:, context:)
   # You shouldn't need to use context, but its fields are explained here:
   # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-  response(body: event, status: 200)
+  if event['path'] == '/token' && event['httpMethod'].upcase == 'POST'
+    generate_token(body: event)
+  elsif event['path'] == '/' && event['httpMethod'].upcase == 'GET'
+    get_token_data(body: event)
+  end
+  # response(body: event, status: 200)
+end
+
+def generate_token(body: event)
+  return response(status: 422) unless body.is_a?(Hash)
+  return response(status: 415) unless body['headers']['Content-Type'] == 'application/json'
+
+  payload = {
+      data: body,
+      exp: Time.now.to_i + 1,
+      nbf: Time.now.to_i
+  }
+  token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
+  response(body:
+  {
+    token: token
+  }, status: 201)
+end
+
+def get_token_data(body: event)
+  2
 end
 
 def response(body: nil, status: 200)
